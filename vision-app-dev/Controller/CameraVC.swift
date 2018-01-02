c262e8e77bb51a893fc97a2415986ed355205099
Adolfo Reyes <adolfo0620@gmail.com>
2018-01-01 19:43:47 -0500

    init project

---
+++ vision-app-dev/Controller/CameraVC.swift
@@ -0,0 +1,25 @@
+//
+//  CameraVC.swift
+//  vision-app-dev
+//
+//  Created by adolfo reyes on 1/1/18.
+//  Copyright © 2018 adolfo reyes. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
